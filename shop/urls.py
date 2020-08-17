f31c7dfd6e81b93700189a95c2ae64d55b65bb20
Mihail <mihan5926@gmail.com>
2020-08-17 17:39:41 +0400

    request CategoryList
    add Serializers and View for request

--- shop/urls.py
+++ shop/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.urls import path, re_path, include
 from django.contrib import admin
-from catalog.views import *
+from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('catalog/', include('catalog.urls')),
 ]
